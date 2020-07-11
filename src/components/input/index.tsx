d32ccfe1d6cee50b05ee818a541ed15cd8fb255b
Marcos Volke <marcos@sizex.com.br>
2020-07-11 08:02:53 -0300

    Instalação, uso do Unform da Rocketseat e adaptação do componente Input pra usar Unform

--- src/components/input/index.tsx
+++ src/components/input/index.tsx
@@ -1,5 +1,6 @@
-import React, { InputHTMLAttributes } from 'react';
+import React, { InputHTMLAttributes, useEffect, useRef } from 'react';
 import { IconBaseProps } from 'react-icons';
+import { useField } from '@unform/core';
 
 import { Container } from './styles';
 
@@ -11,11 +12,27 @@ interface inputProps extends InputHTMLAttributes<HTMLInputElement> {
 }
 
 // hack do react pra q ele entenda q Icon é uma tag, declara o tipo igual o nome da prop mas com a primeira letra maiúscula
-const Input: React.FC<inputProps> = ({ icon: Icon, ...restProps }) => (
-  <Container>
-    { Icon && <Icon size={20} /> }
-    <input { ...restProps }/>
-  </Container>
-);
+const Input: React.FC<inputProps> = ({ name, icon: Icon, ...restProps }) => {
+  // Referência do input no DOM pra dar foco, etc...
+  const inputRef = useRef(null);
+  // Registra no unform cada input
+  const { fieldName, defaultValue, error, registerField } = useField(name);
+
+  // Assim que o componente for exibido em tela:
+  useEffect(() => {
+    registerField({
+      name: fieldName,
+      ref: inputRef.current,
+      path: 'value',
+    });
+  }, [fieldName, registerField]);
+
+  return (
+    <Container>
+      { Icon && <Icon size={20} /> }
+      <input defaultValue={defaultValue} ref={inputRef} { ...restProps }/>
+    </Container>
+  );
+};
 
 export default Input;
--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { FiArrowLeft, FiMail, FiLock, FiUser } from 'react-icons/fi';
+import { Form } from '@unform/web';
 
 import logoImg from '../../assets/logo.svg';
 
@@ -8,22 +9,28 @@ import Button from '../../components/button';
 
 import { Container, Content, Background } from './styles';
 
-const SignUp: React.FC = () => (
+const SignUp: React.FC = () => {
+  function handleSubmit(data: object): void {
+    console.log(data);
+  }
+
+  return (
     <Container>
         <Background />
 
         <Content>
             <img src={logoImg} alt="GoBarber"/>
 
-            <form>
+            {/* Propriedade do Form do Unform para inicializar campos: initialData={{ name: 'Marcos' }} */}
+            <Form onSubmit={handleSubmit}>
                 <h1>Faça seu Cadastro</h1>
 
-                <Input name="nome" type="text" placeholder="Nome" icon={FiUser} />
+                <Input name="name" type="text" placeholder="Nome" icon={FiUser} />
                 <Input name="email" type="text" placeholder="E-mail" icon={FiMail} />
                 <Input name="password" type="password" placeholder="Senha" icon={FiLock} />
 
                 <Button type="submit">Cadastrar</Button>
-            </form>
+            </Form>
 
             <a href="forgot">
                 <FiArrowLeft />
@@ -31,6 +38,7 @@ const SignUp: React.FC = () => (
             </a>
         </Content>
     </Container>
-);
+  );
+};
 
 export default SignUp;
