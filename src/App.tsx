61bbdef79e6d7906ece5aba9189a068d58dcd7e7
Marcos Volke <marcos@sizex.com.br>
2020-07-11 12:44:38 -0300

    Criação de contexto de autenticação global ainda básico

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,17 @@ import React from 'react';
 
 import GlobalStyle from './styles/global';
 import SignIn from './pages/SignIn';
-import SignUp from './pages/SignUp';
+// import SignUp from './pages/SignUp';
+
+import AuthContext from './context/AuthContext';
 
 const App: React.FC = () => (
   <>
-    <SignUp />
+    // Faço com que toda a aplicação tenha acesso ao contexto de autenticação
+    <AuthContext.Provider value={{ name: 'Marcos' }}>
+      <SignIn />
+    </AuthContext.Provider>
+
     <GlobalStyle />
   </>
 );
