9e739fadfcbc8d96faee1355dfec081d192195f6
Marcos Volke <marcos@sizex.com.br>
2020-07-13 08:39:41 -0300

    Ainda no Toast: adicionar e remover na tela

---
+++ src/components/toastContainer/Toast/index.tsx
@@ -0,0 +1,49 @@
+import React, { useEffect } from 'react';
+import { FiAlertCircle, FiXCircle, FiCheckCircle, FiInfo } from 'react-icons/fi'
+
+import { ToastMessage, useToast } from '../../../context/ToastContext';
+
+import { Container } from './styles';
+
+interface ToastProps {
+  message: ToastMessage;
+}
+
+const icons = {
+  info: <FiInfo size={24} />,
+  error: <FiAlertCircle size={24} />,
+  success: <FiCheckCircle size={24} />,
+};
+
+const Toast: React.FC<ToastProps> = ({ message }) => {
+  const { removeToast } = useToast();
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      removeToast(message.id);
+    }, 3000);
+
+    // se eu retorno uma função, ela vai ser executada sempre que meu componente deixar de existir
+    // pra caso o cliente clique em fechar o toast o timer seja interrompido e não solicite novamente o removeToast q já foi solicitado no botão
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [message.id, removeToast]);
+
+  return (
+    <Container type={message.type} hasDescription={!!message.description}>
+      {icons[message.type || 'info']}
+
+      <div>
+        <strong>{message.title}</strong>
+        {message.description && <p>{message.description}</p>}
+      </div>
+
+      <button type="button" onClick={() => removeToast(message.id)}>
+        <FiXCircle size={18} />
+      </button>
+    </Container>
+  );
+}
+
+export default Toast;
--- src/components/toastContainer/styles.ts
+++ src/components/toastContainer/styles.ts
@@ -1,9 +1,4 @@
-import styled, { css } from 'styled-components';
-
-interface ToastProps {
-  type?: 'success' | 'error' | 'info';
-  hasDescription: boolean;
-}
+import styled from 'styled-components';
 
 export const Container = styled.div`
   /* pra ficar solto na tela, por cima de tudo */
@@ -14,74 +9,4 @@ export const Container = styled.div`
   overflow: hidden;
 `;
 
-const toastTypeVariations = {
-  info: css`
-    background: #ebf8ff;
-    color: #3172b7;
-  `,
-  success: css`
-    background: #e6fffa;
-    color: #2e656a;
-  `,
-  error: css`
-    background: #fddede;
-    color: #c53030;
-  `,
-};
 
-export const Toast = styled.div<ToastProps>`
-  width: 360px;
-
-  position: relative;
-  padding: 16px 30px 16px 16px;
-  border-radius: 10px;
-  /* efeito de sombra: */
-  box-shadow: 2px 2px 8px rgba(0, 0, 0, 0.2);
-
-  /* muda alinhamento para horizontal dos elementos: */
-  display: flex;
-
-  /* Toast q antes dele tem um outro toast vai ter margin acima: */
-  & + div {
-    margin-top: 8px;
-  }
-
-  ${props => toastTypeVariations[props.type || 'info']}
-
-  /* pega primeiro svg (dentro do toast) e não pega svg dentro do button */
-  > svg {
-    margin: 4px 12px 0 0;
-  }
-
-  div {
-    /* pra ocupar o tamanho máximo disponível */
-    flex: 1;
-
-    p {
-      margin-top: 4px;
-      font-size: 14px;
-      opacity: 0.8;
-      line-height: 20px;
-    }
-  }
-
-  button {
-    /* o toast é relative justamente pra ter esse absolute dentro */
-    position: absolute;
-    right: 16px;
-    top: 19px;
-    opacity: 0.6;
-    border: 0;
-    background: transparent;
-    color: inherit;
-  }
-
-  ${props => !props.hasDescription && css`
-    align-items: center;
-
-    svg {
-      margin-top: 0;
-
-    }
-  `}
-`;
