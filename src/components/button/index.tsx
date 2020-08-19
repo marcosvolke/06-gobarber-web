302d89ce58cedff812b3af73c78a4d818edf0190
Marcos Volke <marcos@sizex.com.br>
2020-08-19 08:13:59 -0300

    Prop Loading no nosso botao

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -3,12 +3,14 @@ import React, { ButtonHTMLAttributes } from 'react';
 import { Container } from './styles';
 
 // como não vou sobrescrever nenhuma prop, posso declarar um type ao invés de interface
-type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>;
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
+  loading?: boolean;
+};
 
 // children é a prop onde vem o conteúdo da tag - o texto do botão - funciona pra todos
-const Button: React.FC<ButtonProps> = ({ children, ...restProps }) => (
+const Button: React.FC<ButtonProps> = ({ children, loading, ...restProps }) => (
   <Container type="button" {...restProps}>
-    {children}
+    {loading ? 'Carregando...' : children}
   </Container>
 );
 
