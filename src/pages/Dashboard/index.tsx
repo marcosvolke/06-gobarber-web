19f72621e69177539f8e04e53ae15a92f7e453b0
Marcos Volke <marcos@sizex.com.br>
2020-08-20 23:17:17 -0300

    Próximo Agendamento no dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
-import { FiPower } from 'react-icons/fi';
+import { FiPower, FiClock } from 'react-icons/fi';
 
 import {
   Container,
   Header,
   HeaderContent,
   Profile,
+  Content,
+  Schedule,
+  NextAppointment,
+  Calendar,
 } from './styles';
 
 import logoImg from '../../assets/logo.svg';
@@ -34,6 +38,30 @@ const Dashboard: React.FC = () => {
           </button>
         </HeaderContent>
       </Header>
+
+      <Content>
+        <Schedule>
+          <h1>Horários Agendados</h1>
+          <p>
+            <span>Hoje</span>
+            <span>Dia 6</span>
+            <span>Segunda-feira</span>
+          </p>
+
+          <NextAppointment>
+            <strong>Atendimento a seguir</strong>
+            <div>
+              <img src="https://avatars1.githubusercontent.com/u/67933043?s=460&u=8ef84d6689b9981f7cc6fc4a3fe2cdf7960d302d&v=4" alt="Marcos Volke"/>
+              <strong>Marcos Volke</strong>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+            </div>
+          </NextAppointment>
+        </Schedule>
+        <Calendar />
+      </Content>
     </Container>
   );
 };
