16a97ed91c00a958114afecdc04fe7189628de34
Marcos Volke <marcos@sizex.com.br>
2020-08-22 10:59:41 -0300

    Dashboard - nao permitir clicar em dia desabilitado do calendar

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -48,7 +48,7 @@ const Dashboard: React.FC = () => {
   const [appointments, setAppointments] = useState<Appointment[]>([]);
 
   const handleDateChange = useCallback((day: Date, modifiers: DayModifiers) => {
-    if (modifiers.available) {
+    if (modifiers.available && !modifiers.disabled) {
       setSelectedDate(day);
     }
   }, []);
